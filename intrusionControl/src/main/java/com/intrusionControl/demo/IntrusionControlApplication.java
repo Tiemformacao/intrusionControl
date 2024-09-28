5f09257cd3ea494be4bdea4efe4acfcbac874cb8
Tiemformacao <tiemformacao@outlook.com>
2024-09-28 12:58:20 -0300

    Project created

---
+++ intrusionControl/src/main/java/com/intrusionControl/demo/IntrusionControlApplication.java
@@ -0,0 +1,13 @@
+package com.intrusionControl.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntrusionControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntrusionControlApplication.class, args);
+	}
+
+}
---
+++ intrusionControl/src/test/java/com/intrusionControl/demo/IntrusionControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intrusionControl.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntrusionControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
